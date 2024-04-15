68d001559edf0744a278371e33983fc5bac75311
TalyssonMarley <talyssonmarley01@gmail.com>
2024-04-15 10:53:49 -0300

    insert script register btn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,14 @@
 const viewPlansBtn = document.querySelectorAll('.viewplan-btn')[0];
+const registrationBtn = document.querySelectorAll('.registration-btn')[0];
 
 viewPlansBtn.addEventListener('click', showSectionPlan);
+registrationBtn.addEventListener('click', showRegister);
 
 function showSectionPlan() {
     window.scrollTo(0, 1175)
+};
+
+function showRegister() {
+    console.log('certo')
+    window.location.href = "https://www.youtube.com/shorts/b6fZzvx6osw";
 };
