0d79d117061c6b1ad6617ecc973a0f8bc30e9777
TalyssonMarley <talyssonmarley01@gmail.com>
2024-04-15 10:42:57 -0300

    insert script view plans button

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const viewPlansBtn = document.querySelectorAll('.viewplan-btn')[0];
+
+viewPlansBtn.addEventListener('click', showSectionPlan);
+
+function showSectionPlan() {
+    window.scrollTo(0, 1175)
+};
